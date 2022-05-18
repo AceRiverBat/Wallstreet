ed0c9d9ddc285d1040f99da86c0f1750159dae1d
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-05-18 15:34:29 +0200

    header fini

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,4 @@
 import '../App.css';
-import profil from '../img/Profil.png';
 import ajouter from '../img/Ajouter.png';
 
 function Header() {
@@ -7,9 +6,7 @@ function Header() {
     <header>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="#">
-            <img src={profil} className="App-icon" alt="profil" />
-          </a>
+          <h4>Bonjour <strong>{localStorage.getItem('user')}</strong> </h4>
           <a className="navbar-brand" href="#">
             <img src={ajouter} className="App-icon" alt="Ajouter" />
           </a>
