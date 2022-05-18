4d272f11c2de632564b11f5b5f1b9b1320fbff2b
Sarah Tahar <92088786+AceRiverBat@users.noreply.github.com>
2022-05-18 14:35:37 +0200

    first

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+export const Footer = () => {
+  return (
+      <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+          <a className="navbar-brand">Footer</a>
+        </div>
+      </nav>
+  );
+};
---
+++ src/components/Items.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useState } from 'react';
+
+export default function Items() {
+
+    const [monState, setMonState] = useState([]);
+
+    return (
+        <div>
+            <h1>HELLO</h1>
+        </div>
+    )
+};
